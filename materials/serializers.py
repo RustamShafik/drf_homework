b5cd07f79506063ff28de4e462df7bb56fc1c72f
Rustam Shafikov <mitaleko@gmail.com>
2025-07-16 10:34:15 +0500

    Задание 1

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-
+from .validators import validate_youtube_link
 from materials.models import Course, Lesson
+from rest_framework import serializers
 
 
 class LessonSerializer(ModelSerializer):
+    video_link = serializers.CharField(
+        allow_blank=True,
+        required=False,
+        validators=[validate_youtube_link]
+    )
     class Meta:
         model = Lesson
         fields = "__all__"
---
+++ materials/validators.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+import re
+
+def validate_youtube_link(value):
+    pattern = r"(https?://)?(www\.)?(youtube\.com|youtu\.be)/"
+    if value and not re.search(pattern, value):
+        raise serializers.ValidationError(
+            "Разрешены только ссылки на YouTube."
+        )
+    return value
