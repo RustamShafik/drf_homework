2d9136845f311402bf895bdffee128f382dda665
Rustam Shafikov <mitaleko@gmail.com>
2025-07-28 14:00:01 +0500

    Задание 2 работает, уведомление получено

--- materials/admin.py
+++ materials/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Course, Lesson
+from .models import Subscription
 
 admin.site.register(Course)
 admin.site.register(Lesson)
+admin.site.register(Subscription)
